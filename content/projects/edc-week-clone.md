a5a8a06a2b73cec72c6863667e0af1276c551a5f
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-12-05 02:46:15 -0800

    feat: add initial skeleton, nav WIP

---
+++ content/projects/edc-week-clone.md
@@ -0,0 +1,6 @@
+---
+title: "EDC Week Clone"
+draft: false
+---
+
+A front-end focused project built with Vue, Vue Router, and Tailwind CSS. I built this side project because my new position's tech stack uses Vue instead of what I learned in my bootcamp (React). I wanted to build my own project to get the ins and outs and fundamentals down and I like Insomniac events, so I decided to make a clone of its 2022 EDC Week page.
---
+++ static/js/main.js
@@ -0,0 +1,88 @@
+const toggleButton = document.querySelector('#hamburger');
+const line1 = document.querySelector('#line1');
+const line2 = document.querySelector('#line2');
+const line3 = document.querySelector('#line3');
+const logo = document.querySelector('.logo');
+const menuItems = document.querySelector('.menu-items');
+const menuItemUL = document.querySelector('.menu-item-ul');
+const bodyElement = document.querySelector('#body');
+
+let clicked = false;
+
+const showLogo = () => {
+  logo.className = 'logo';
+};
+
+const hideLogo = () => {
+  logo.className = 'hidden';
+};
+
+const showMenuItems = () => {
+  menuItems.className = 'translate';
+};
+
+const hideMenuItems = () => {
+  menuItems.className = 'menu-items';
+};
+
+const linesClicked = () => {
+  line1.className = 'line line1 line1-clicked';
+  line2.className = 'line line2 line2-clicked';
+  line3.className = 'line line3 line3-clicked';
+};
+
+const linesUnclicked = () => {
+  line1.className = 'line line1';
+  line2.className = 'line line2';
+  line3.className = 'line line3';
+};
+
+const overflowYHidden = () => {
+  bodyElement.className = 'overflow-y-hidden';
+};
+
+const overFlowAuto = () => {
+  bodyElement.className = 'overflow-auto';
+};
+
+const toggleClicked = () => {
+  hideLogo();
+  showMenuItems();
+  linesClicked();
+  overflowYHidden();
+};
+
+const toggleUnclicked = () => {
+  showLogo();
+  hideMenuItems();
+  linesUnclicked();
+  overFlowAuto();
+};
+
+const toggleClickEventHandler = () => {
+  clicked = !clicked;
+  if (clicked === true) {
+    toggleClicked();
+  } else {
+    toggleUnclicked();
+  }
+};
+
+toggleButton.addEventListener('click', toggleClickEventHandler);
+
+const menuItemClicked = () => {
+  if (event.target.tagName === 'A') {
+    toggleUnclicked();
+    clicked = false;
+  }
+};
+
+menuItemUL.addEventListener('click', menuItemClicked);
+
+const handleResize = () => {
+  if (screen.width > 630) {
+    toggleUnclicked();
+  }
+};
+
+document.addEventListener('resize', handleResize);
