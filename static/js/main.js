9eec3d3db9e3a4f1d832673e3d4df09a1bc8af03
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-12-11 19:30:01 -0800

    feat: add off canvas nav

---
+++ static/js/main.js
@@ -0,0 +1,91 @@
+const toggleButton = document.querySelector('#hamburger');
+const line1 = document.querySelector('#line1');
+const line2 = document.querySelector('#line2');
+const line3 = document.querySelector('#line3');
+const logo = document.querySelector('#logo');
+// const menuItems = document.querySelector('.menu-items');
+// const menuItemUL = document.querySelector('.menu-item-ul');
+const menuItems = document.querySelector('#menu-items');
+const menuItemUL = document.querySelector('#menu-item-ul');
+const bodyElement = document.querySelector('#body');
+
+let clicked = false;
+
+const showLogo = () => {
+  logo.className = ''
+};
+
+const hideLogo = () => {
+  logo.className = 'd-none';
+};
+
+const showMenuItems = () => {
+  menuItems.className = 'translate';
+};
+
+const hideMenuItems = () => {
+  menuItems.className = 'menu-items';
+};
+
+const linesClicked = () => {
+  line1.className = 'line line1 line1-clicked';
+  line2.className = 'line line2 line2-clicked';
+  line3.className = 'line line3 line3-clicked';
+};
+
+const linesUnclicked = () => {
+  line1.className = 'line line1';
+  line2.className = 'line line2';
+  line3.className = 'line line3';
+};
+
+const overflowYHidden = () => {
+  bodyElement.className = 'overflow-y-hidden';
+};
+
+const overFlowAuto = () => {
+  bodyElement.className = 'overflow-auto';
+};
+
+const toggleClicked = () => {
+  hideLogo();
+  showMenuItems();
+  linesClicked();
+  overflowYHidden();
+};
+
+const toggleUnclicked = () => {
+  showLogo();
+  hideMenuItems();
+  linesUnclicked();
+  overFlowAuto();
+};
+
+const handleToggleClick = () => {
+  clicked = !clicked;
+  if (clicked === true) {
+    toggleClicked();
+  } else {
+    toggleUnclicked();
+  }
+};
+
+
+toggleButton.addEventListener('click', handleToggleClick);
+
+const menuItemClicked = () => {
+  if (event.target.tagName === 'A') {
+    toggleUnclicked();
+    clicked = false;
+  }
+};
+
+menuItemUL.addEventListener('click', menuItemClicked);
+
+const handleResize = () => {
+  if (screen.width > 630) {
+    toggleUnclicked();
+  }
+};
+
+document.addEventListener('resize', handleResize);
